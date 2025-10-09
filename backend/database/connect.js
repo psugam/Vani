3ad74af10679c93de2791fbf8dc7dd10e72d85c7
psugam <pokharelsugam19@gmail.com>
2025-10-09 18:40:50 +0545

    cors

--- backend/database/connect.js
+++ backend/database/connect.js
@@ -4,16 +4,23 @@ dotenv.config();
 
 const dbURI=`mongodb+srv://${process.env.USER_NAME}:${process.env.PASS_WORD}@cluster0.w5q0xla.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 
-async function connectDB(){
-try{
-    await mongoose.connect(dbURI, {
-        // no options for now will add later
-    })
-    console.log('MongoDB connected successfully.')
-}catch(error){
-    console.log(error);
-    process.exit(1);
-}
+let cached = global.mongoose;
+if (!cached) cached = global.mongoose = { conn: null, promise: null };
+
+async function connectDB() {
+  if (cached.conn) {
+    return cached.conn; // return existing connection
+  }
+
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(dbURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }).then(mongoose => mongoose);
+  }
+
+  cached.conn = await cached.promise;
+  return cached.conn;
 }
 
-module.exports=connectDB;
+module.exports = connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors=require('cors')
 const dotenv =require('dotenv');
 const connectDB = require('./database/connect');
 dotenv.config();
